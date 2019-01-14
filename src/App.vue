8de55a9d92d88c31bbdc33b8b54176ac1e407b45
chenxin <chenxin@funtsui.com>
2019-01-14 11:17:02 +0700

    feat: 移除未完成排序

--- src/App.vue
+++ src/App.vue
@@ -44,14 +44,7 @@ export default {
       tabs: [
         { label: '冒泡排序', type: 'bubble' },
         { label: '选择排序', type: 'selection' },
-        { label: '插入排序', type: 'insertion' },
-        { label: '希尔排序', type: 'name3' },
-        { label: '归并排序', type: 'name4' },
-        { label: '快速排序', type: 'name5' },
-        { label: '计数排序', type: 'name6' },
-        { label: '基数排序', type: 'name7' },
-        { label: '堆排序', type: 'name8' },
-        { label: '桶排序', type: 'name9' }
+        { label: '插入排序', type: 'insertion' }
       ],
       curTab: 'bubble'
     }
